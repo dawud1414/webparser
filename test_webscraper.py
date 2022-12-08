ecf1bcffe66ccebd32de13bdbbecad30c3408bcb
dawud1414 <92131857+dawud1414@users.noreply.github.com>
2022-12-07 19:09:29 -0500

    Delete test_webscraper.py

--- test_webscraper.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-#using pathlib check to see if file exist
-
-file_name = Path("new.csv")
-if file_name.exists():
-    print("exists") 
-else:
-    print("does not exist") 
